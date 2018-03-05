b9f535562c90b0c1928f2142699ecc258eb60af3
Ofir <ofir.asor@sapiens.com>
2018-03-05 14:00:15 +0200

    spring-boot generated app

---
+++ src/main/java/com/sapiens/bdms/decisionexecutor/DecisionExecutorApplication.java
@@ -0,0 +1,12 @@
+package com.sapiens.bdms.decisionexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionExecutorApplication.class, args);
+	}
+}
